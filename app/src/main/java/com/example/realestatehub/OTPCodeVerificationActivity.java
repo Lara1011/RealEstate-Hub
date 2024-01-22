e3efda007fc9848ea07f90132b0605c29db0651b
marwanhreish <62513189+marwanhresh@users.noreply.github.com>
2024-01-22 17:40:48 +0200

    Add files via upload

---
+++ app/src/main/java/com/example/realestatehub/OTPCodeVerificationActivity.java
@@ -0,0 +1,38 @@
+package com.example.realestatehub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class OTPCodeVerificationActivity extends AppCompatActivity {
+
+    private Button verifyBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //=========Device Bar Color=========
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(getResources().getColor(R.color.white));
+        setContentView(R.layout.activity_otpcode_verification);
+
+        initUI();
+    }
+
+    private void initUI(){
+        verifyBtn = findViewById(R.id.verify_btn);
+
+        verifyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(OTPCodeVerificationActivity.this, SetNewPasswordActivity.class));
+            }
+        });
+    }
+}
