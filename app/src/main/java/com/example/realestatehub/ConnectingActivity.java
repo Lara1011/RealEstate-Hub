ead2c2f7322e7706bbaf6f408a9e33b6ee0f4f33
malak <malak.qeedan@hotmail.com>
2024-02-03 02:13:15 +0200

    Manage to logIn via Google, Saving user data in real time.

--- app/src/main/java/com/example/realestatehub/ConnectingActivity.java
+++ app/src/main/java/com/example/realestatehub/ConnectingActivity.java
@@ -6,8 +6,31 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+import javax.annotation.Nullable;
 
 public class ConnectingActivity extends AppCompatActivity implements View.OnClickListener {
     private Button signInButton;
@@ -17,11 +40,15 @@ public class ConnectingActivity extends AppCompatActivity implements View.OnClic
     private Button microsoftButton;
     private Button signUpButton;
     private Intent intent;
+    private FirebaseAuth auth;
+    private FirebaseDatabase database;
+    private GoogleSignInClient GoogleSignInClient;
+    int RC_SIGN_IN = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_connecting);
+        setContentView(R.layout.activity1_connecting);
         //=========Device Bar Color=========
         Window window = getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
@@ -31,6 +58,14 @@ public class ConnectingActivity extends AppCompatActivity implements View.OnClic
     }
 
     private void initUI() {
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        GoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
         signInButton = findViewById(R.id.signInButton);
         forgotPasswordButton = findViewById(R.id.forgotPasswordButton);
         facebookButton = findViewById(R.id.facebookButton);
@@ -54,25 +89,85 @@ public class ConnectingActivity extends AppCompatActivity implements View.OnClic
         finish();
     }
 
+
     @Override
     public void onClick(View v) {
-        //            case R.id.signInButton:
-        //                break;
-        //            case R.id.forgotPasswordButton:
-        //                intent = new Intent(this, ForgotPasswordActivity.class);
-        //                startActivity(intent);
-        //                finish();
-        //                break;
-        //            case R.id.facebookButton:
-        //                break;
-        //            case R.id.googleButton:
-        //                break;
-        //            case R.id.microsoftButton:
-        //                break;
-        if (v.getId() == R.id.signUpButton) {
+        int id = v.getId();
+        if (id == R.id.signInButton) {
+            regularSignIn();
+        }
+        else if (id == R.id.forgotPasswordButton) {
+            intent = new Intent(this, ForgotPasswordActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        else if (id == R.id.googleButton) {
+            googleSignIn();
+        }
+        else if (id == R.id.signUpButton) {
             intent = new Intent(this, SignUpActivity.class);
             startActivity(intent);
             finish();
         }
+    }
+
+    private void regularSignIn() {
+
+    }
+
+    private void googleSignIn() {
+        Intent signInIntent = GoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (Exception e) {
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        auth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    FirebaseUser user = auth.getCurrentUser();
+                    short epsTime = 1000;
+                    long dateCreate = Objects.requireNonNull(user.getMetadata()).getCreationTimestamp();
+                    long dateLastLogin = user.getMetadata().getLastSignInTimestamp();
+                    long currTime = System.currentTimeMillis();
+                    HashMap<String, Object> map = new HashMap<>();
+                    map.put("id", user.getUid());
+                    map.put("username", user.getDisplayName());
+                    map.put("profilePic", user.getPhotoUrl().toString());
+                    map.put("email", user.getEmail());
+                    map.put("phoneNumber", user.getPhoneNumber());
+                    map.put("created", dateCreate);
+                    map.put("lastLogin", dateLastLogin);
+
+                    database.getReference().child("Registered Users").child(user.getUid()).setValue(map);
+                    Intent intent;
+                    if (currTime < dateCreate + epsTime) {
+                        intent = new Intent(ConnectingActivity.this, SignUpActivity.class);
+                    } else {
+                        intent = new Intent(ConnectingActivity.this, HomePageActivity.class);
+                    }
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(ConnectingActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
