344eff717b700eb71264e61ac1bc26c3d67661f3
marwanhreish <62513189+marwanhresh@users.noreply.github.com>
2024-02-12 02:07:37 +0200

    Update PostAdapter.java

--- app/src/main/java/com/example/realestatehub/HomeFragments/ReadPost/PostAdapter.java
+++ app/src/main/java/com/example/realestatehub/HomeFragments/ReadPost/PostAdapter.java
@@ -12,31 +12,25 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.example.realestatehub.R;
+import java.util.HashMap;
 import java.util.List;
 
-// Define the PostAdapter class that extends RecyclerView.Adapter for custom ViewHolder.
 public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
-    // Declare a List to hold PostDetails objects.
-    private List<PostDetails> postList;
-    // Declare an OnItemClickListener interface to handle item clicks.
+    private List<HashMap<String, Object>> postList;
     private OnItemClickListener mListener;
 
-    // Define the OnItemClickListener interface.
     public interface OnItemClickListener {
         void onItemClick(int position);
     }
 
-    // Method to set the listener for item clicks.
     public void setOnItemClickListener(OnItemClickListener listener) {
         mListener = listener;
     }
 
-    // Constructor to initialize the PostAdapter with a list of PostDetails.
-    public PostAdapter(List<PostDetails> postList) {
+    public PostAdapter(List<HashMap<String, Object>> postList) {
         this.postList = postList;
     }
 
-    // Method to inflate the item layout and create ViewHolder.
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -44,24 +38,22 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
         return new ViewHolder(view);
     }
 
-    // Method to bind data to the ViewHolder elements based on the position of the item in the list.
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        PostDetails post = postList.get(position);
-        holder.nameTextView.setText(getSafeValue(post.getName()));
-        if (post.userName != null)
-            holder.userTextView.setText(getSafeValue("by " + post.userName));
+        HashMap<String, Object> post = postList.get(position);
+        holder.nameTextView.setText(getSafeValue((String) post.get("name")));
+        if (post.get("userName") != null)
+            holder.userTextView.setText(getSafeValue("by " + (String) post.get("userName")));
 
-        // Use Glide library to load an image into the ImageView, or set a default image if URL is null or empty.
-        if (post.getPhotoUrl() != null && !post.getPhotoUrl().isEmpty()) {
+        if (post.get("photoUrl") != null && !((String) post.get("photoUrl")).isEmpty()) {
             Glide.with(holder.itemView.getContext())
-                    .load(post.getPhotoUrl())
+                    .load((String) post.get("photoUrl"))
                     .into(holder.imageView);
         } else {
-             holder.imageView.setImageResource(R.drawable.picture_img);
+            holder.imageView.setImageResource(R.drawable.picture_img);
+            // holder.imageView.setVisibility(View.GONE);
         }
 
-        // Set an OnClickListener to the "view details" button to start a new activity showing post details.
         holder.viewDetailsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -73,13 +65,11 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
         });
     }
 
-    // Method to return the total number of items in the list.
     @Override
     public int getItemCount() {
         return postList.size();
     }
 
-    // Define the ViewHolder class that holds references to the UI components within the RecyclerView item.
     public static class ViewHolder extends RecyclerView.ViewHolder {
         public TextView nameTextView;
         public TextView userTextView;
@@ -95,7 +85,6 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
         }
     }
 
-    // Helper method to return a non-null String value.
     private String getSafeValue(String value) {
         return value != null ? value : "";
     }
