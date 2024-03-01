370ad5d66f280cb9d752cc6d3adf9d99339b06c3
marwanhreish <62513189+marwanhresh@users.noreply.github.com>
2024-03-01 13:59:37 +0200

    Update PostDetailsActivity.java

--- app/src/main/java/com/example/realestatehub/HomeFragments/ReadPost/PostDetailsActivity.java
+++ app/src/main/java/com/example/realestatehub/HomeFragments/ReadPost/PostDetailsActivity.java
@@ -9,14 +9,82 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.example.realestatehub.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.HashMap;
+import java.util.Map;
 
 public class PostDetailsActivity extends AppCompatActivity {
+    private void addToRecentlyDB(String itemId){
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("UserViewedItems"); // "UserViewedItems"는 최근 본 항목을 저장하는 노드 이름입니다.
+        // 현재 날짜를 가져오는 코드
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentDate = dateFormat.format(calendar.getTime());
+
+        HashMap<String, Object> viewedItemMap = new HashMap<>();
+        viewedItemMap.put("itemId", itemId);
+        viewedItemMap.put("viewedDate", currentDate);
+        // 업데이트할 데이터를 포함하는 Map을 생성합니다.
+        Map<String, Object> updateMap = new HashMap<>();
+        updateMap.put(itemId, viewedItemMap);
+
+        reference.child(uid).updateChildren(updateMap)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // 업데이트 성공 시 처리할 코드
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // 업데이트 실패 시 처리할 코드
+                    }
+                });
+    }
+    private void addToPurchasedDB(String itemId){
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("UserReachedItems"); // "UserPurchasedItems"는 최근 구매한 항목을 저장하는 노드 이름입니다.
+        // 현재 날짜를 가져오는 코드
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentDate = dateFormat.format(calendar.getTime());
+
+        HashMap<String, Object> purchasedItemMap = new HashMap<>();
+        purchasedItemMap.put("itemId", itemId);
+        purchasedItemMap.put("reachedDate", currentDate);
+        // 업데이트할 데이터를 포함하는 Map을 생성합니다.
+        Map<String, Object> updateMap = new HashMap<>();
+        updateMap.put(itemId, purchasedItemMap);
+
+        reference.child(uid).updateChildren(updateMap)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // 업데이트 성공 시 처리할 코드
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // 업데이트 실패 시 처리할 코드
+                    }
+                });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +95,8 @@ public class PostDetailsActivity extends AppCompatActivity {
         if (intent != null) {
             HashMap<String, Object> post = (HashMap<String, Object>) intent.getSerializableExtra("postDetails");
             if (post != null) {
+                addToRecentlyDB(  getSafeValue(intent.getStringExtra("postId")) );
+
                 TextView userNameTextView = findViewById(R.id.userNameTextView);
                 TextView phoneNumberTextView = findViewById(R.id.phoneNumberTextView);
                 TextView nameTextView = findViewById(R.id.nameTextView);
@@ -81,6 +151,7 @@ public class PostDetailsActivity extends AppCompatActivity {
                 callButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
+                        addToPurchasedDB(  getSafeValue(intent.getStringExtra("postId")) );
                         String phoneNumber = getSafeValue(post.get("phoneNumber"));
                         Intent dialIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumber));
                         startActivity(dialIntent);
@@ -91,6 +162,7 @@ public class PostDetailsActivity extends AppCompatActivity {
                 whatsappButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
+                        addToPurchasedDB(  getSafeValue(intent.getStringExtra("postId")) );
                         String phoneNumber = getSafeValue(post.get("phoneNumber"));
                         Intent whatsappIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://api.whatsapp.com/send?phone=" + phoneNumber));
                         startActivity(whatsappIntent);
